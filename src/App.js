5c020b38e3532eab250d6077179e351c59d3adaa
MungaiKeren <wambukeren@gmail.com>
2023-05-23 15:20:16 +0300

    Start things off

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Welcome from "./components/Welcome";
+import ChatBox from "./components/ChatBox";
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState(false)
+    return (
+        <div className="App">
+            {!user ? (
+                <Welcome />
+            ): 
+            (
+            <>
+            <ChatBox />
+            </>
+            )}
+        </div>
+    )
 }
 
 export default App;
